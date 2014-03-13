ae29bc1556e7290ec11b67ed77ca44c1a5df4222
LukaszCaputa <lukasz@lukaszcaputa.eu>
2014-03-13 20:07:14 +0100

    Upgrades and fixes
    corrected pages - new contacts etc.
    removed unnecessary controllers

--- js/main.js
+++ js/main.js
@@ -14,29 +14,22 @@
 			// route for the about page
 			.when('/about', {
 				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
+				controller  : 'mainController'
 			})
 
 			// route for the contact page
 			.when('/contact', {
 				templateUrl : 'pages/contact.html',
-				controller  : 'contactController'
+				controller  : 'mainController'
 			});
 	});
 
 	// create the controller and inject Angular's $scope
 	sampleApp.controller('mainController', function($scope) {
 		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
-		$scope.test = 'O ja jebie !';
+		$scope.message = '';
+		
 	});
 
-	sampleApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
-		$scope.kiki = '0..1..2..';
-	});
 
-	sampleApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
-	});
 
